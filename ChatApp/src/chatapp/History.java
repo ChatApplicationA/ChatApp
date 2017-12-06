3bdba9c9a568223383ea16523c566e3099d6fa20
Elfends <31990369+Elfends@users.noreply.github.com>
2017-12-06 23:46:52 +0700

    -

--- ChatApp/src/chatapp/History.java
+++ ChatApp/src/chatapp/History.java
@@ -1,8 +1,15 @@
 package chatapp;
 
+import java.io.*;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 import org.w3c.dom.*;
+import ui.HistoryFrame;
+import javax.swing.table.DefaultTableModel;
 
 public class History {
 
@@ -26,8 +33,46 @@ public class History {
 			Element _recipient = doc.createElement("recipient");
 			_recipient.setTextContent(msg.recipient);
 			Element _time = doc.createElement("time");
-			_time.setTextContent(msg.time);
+			_time.setTextContent(time);
+			message.appendChild(_sender);
+			message.appendChild(_content);
+			message.appendChild(_recipient);
+			message.appendChild(_time);
+			data.appendChild(message);
+			TransformerFactory transformerFactory = TransformerFactory.newInstance();
+			Transformer transformer = transformerFactory.newTransformer();
+			DOMSource source = new DOMSource(doc);
+			StreamResult result = new StreamResult(new File(filePath));
+			transformer.transform(source, result);
 		} catch (Exception ex) {
+			System.out.println("Exceptionmodify xml");
 		}
+	}
+
+	public void FillTable(HistoryFrame frame) {
+		DefaultTableModel model = (DefaultTableModel) frame.jTable1.getModel();
+		try {
+			File fXmlFile = new File(filePath);
+			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+			Document doc = dBuilder.parse(fXmlFile);
+			doc.getDocumentElement().normalize();
+			NodeList nList = doc.getElementsByTagName("message");
+			for (int temp = 0; temp < nList.getLength(); temp++) {
+				Node nNode = nList.item(temp);
+				if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+					Element eElement = (Element) nNode;
+					model.addRow(new Object[]{getTagValue("sender", eElement), getTagValue("content", eElement), getTagValue("recipient", eElement), getTagValue("time", eElement)});
+				}
+			}
+		} catch (Exception ex) {
+			System.out.println("Filling Exception");
+		}
+	}
+
+	public static String getTagValue(String sTag, Element eElement) {
+		NodeList nlList = eElement.getElementsByTagName(sTag).item(0).getChildNodes();
+		Node nValue = (Node) nlList.item(0);
+		return nValue.getNodeValue();
 	}
 }
--- ChatApp/src/chatapp/Message.java
+++ ChatApp/src/chatapp/Message.java
@@ -3,27 +3,32 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.socket;
+package chatapp;
+
 import java.io.Serializable;
+
 /**
  *
  * @author Agit
  */
-public class Message implements Serializable{
+public class Message implements Serializable {
 
-    /**
-     * @param args the command line arguments
-     */
-    private static final long serialVersionUID = 1L;
-    public String type, sender, content, recipient;
-    
-    public Message (String type, String sender, String content, String recipient){
-        this.type=type; this.sender=sender; this.content=content; this.recipient=recipient;
-    }
-    
-    @Override
-    public String toString() {
-        return "{type='"+type+"', sender='"+sender+"', content='"+content+"',recipient='"+recipient+"'}";
-    }
-    
+	/**
+	 * @param args the command line arguments
+	 */
+	private static final long serialVersionUID = 1L;
+	public String type, sender, content, recipient;
+
+	public Message(String type, String sender, String content, String recipient) {
+		this.type = type;
+		this.sender = sender;
+		this.content = content;
+		this.recipient = recipient;
+	}
+
+	@Override
+	public String toString() {
+		return "{type='" + type + "', sender='" + sender + "', content='" + content + "',recipient='" + recipient + "'}";
+	}
+
 }
